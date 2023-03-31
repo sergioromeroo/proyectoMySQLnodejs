39e65091386739b1e862faf23ccf064ea378850c
sergioromeroo <manusergio96@gmail.com>
2023-03-31 13:09:03 -0300

    traemos datos a la vista index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,18 +2,106 @@ const database = require('../database/models')
 
 module.exports = {
     index : (req,res) => {
-        var datos = database.User.findAll() 
-        Promise.all([datos])
-        .then(([datos]) => {
-            return res.send(datos)
+        var datos = database.Product.findAll() 
+        let buzos = database.Category.findOne({
+            where : {
+                name : 'buzos'
+            },
+            include : [
+                {
+                    association : 'products',
+                    include : [
+                        {association : 'images'}
+                    ]
+                }
+            ]
+        });
+
+        let camperas = database.Category.findOne({
+            where : {
+                name : 'camperas'
+            },
+            include : [
+                {
+                    association : 'products',
+                    include : [
+                        {association : 'images'}
+                    ]
+                }
+            ]
+        });
+
+        let pantalones = database.Category.findOne({
+            where : {
+                name : 'pantalones'
+            },
+            include : [
+                {
+                    association : 'products',
+                    include : [
+                        {association : 'images'}
+                    ]
+                }
+            ]
+        });
+
+        let calzados = database.Category.findOne({
+            where : {
+                name : 'calzados'
+            },
+            include : [
+                {
+                    association : 'products',
+                    include : [
+                        {association : 'images'}
+                    ]
+                }
+            ]
+        });
+
+        let remeras = database.Category.findOne({
+            where : {
+                name : 'remeras'
+            },
+            include : [
+                {
+                    association : 'products',
+                    include : [
+                        {association : 'images'}
+                    ]
+                }
+            ]
+        });
+
+        let camisas = database.Category.findOne({
+            where : {
+                name : 'camisas'
+            },
+            include : [
+                {
+                    association : 'products',
+                    include : [
+                        {association : 'images'}
+                    ]
+                }
+            ]
+        });
+        Promise.all([buzos,camperas,pantalones,calzados,remeras,camisas])
+        .then(([buzos,camperas,pantalones,calzados,remeras,camisas]) => {
+
+            return res.render('index',{
+                datos,
+                buzos : buzos.products,
+                camperas : camperas.products,
+                pantalones : pantalones.products,
+                calzados : calzados.products,
+                remeras : remeras.products,
+                camisas : camisas.products
+                //return res.send(buzos)
+            })
+
         })
-        //res.send(productosdb) con esto lo veo en chrome
-/*         return res.render('index',{
-            productos : productosdb, //le cambio nombre a mi array
-            abrigos : productosdb.filter(producto => producto.categoria === "abrigos"),
-            camisetas : productosdb.filter(producto => producto.categoria === "camisetas"),
-            pantalones : productosdb.filter(producto => producto.categoria === "pantalon"),
-        }) */
+
     },
     products : (req,res) => {
         return res.render('productos')
